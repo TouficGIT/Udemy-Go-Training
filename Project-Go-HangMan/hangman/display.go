2165ad7fe4244af0bb10013bd53dbd6ae69558e1
TouficGIT <meffe.emilien@gmail.com>
2019-04-08 21:03:35 +0200

    Hangman project: display func - update

--- Project-Go-HangMan/hangman/display.go
+++ Project-Go-HangMan/hangman/display.go
@@ -3,8 +3,7 @@ package hangman
 import "fmt"
 
 func DrawWelcome() {
-	fmt.Println(` 
-	              _______  _        _______  _______  _______  _       
+	fmt.Println(`     
 		|\     /|(  ___  )( (    /|(  ____ \(       )(  ___  )( (    /|
 		| )   ( || (   ) ||  \  ( || (    \/| () () || (   ) ||  \  ( |
 		| (___) || (___) ||   \ | || |      | || || || (___) ||   \ | |
@@ -107,5 +106,30 @@ func drawTurns(l int) {
 
 func drawState(g *Game, guess string) {
 	fmt.Print("Guessed: ")
+	drawLetters(g.FoundLetters)
 
+	fmt.Print("Used: ")
+	drawLetters(g.UsedLetters)
+
+	switch g.State {
+	case "goodGuess":
+		fmt.Print("Good guess!")
+	case "alreadyGuessed":
+		fmt.Printf("Letter '%s' was already used", guess)
+	case "badGuess":
+		fmt.Printf("Bad guess, '%s' is not in the word", guess)
+	case "lost":
+		fmt.Print("You lost :( the word was:")
+		drawLetters(g.Letters)
+	case "won":
+		fmt.Print("You won! The word was:")
+		drawLetters(g.Letters)
+	}
+}
+
+func drawLetters(l []string) {
+	for _, c := range l {
+		fmt.Printf("%v: ", c)
+	}
+	fmt.Println()
 }
