867135f9ae444c634780e481e069338bc7a3745d
TouficGIT <meffe.emilien@gmail.com>
2019-04-16 06:12:43 +0200

    project: dictionary and ddb project with badger

---
+++ Project-Go-Dictionnary/main.go
@@ -0,0 +1,49 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"os"
+
+	"Training.go/Project-Go-Dictionnary/dictionary"
+)
+
+func main() {
+	action := flag.String("action", "list", "Action to perform on the dictionary")
+	d, err := dictionary.New("./badger")
+	handleErr(err)
+	defer d.Close()
+	flag.Parse()
+	switch *action {
+	case "list":
+		actionList(d)
+	case "add":
+		actionAdd(d, flag.Args())
+	default:
+		fmt.Printf("Unknown action: %v\n", *action)
+	}
+}
+
+func actionList(d *dictionary.Dictionary) {
+	words, entries, err := d.List()
+	handleErr(err)
+	fmt.Println("Dictionary content")
+	for _, word := range words {
+		fmt.Println(entries[word])
+	}
+}
+
+func actionAdd(d *dictionary.Dictionary, args []string) {
+	word := args[0]
+	definition := args[1]
+	err := d.Add(word, definition)
+	handleErr(err)
+	fmt.Printf("'%v' added to the dictionary\n", word)
+}
+
+func handleErr(err error) {
+	if err != nil {
+		fmt.Printf("Dictionary error: %v\n", err)
+		os.Exit(1)
+	}
+}
