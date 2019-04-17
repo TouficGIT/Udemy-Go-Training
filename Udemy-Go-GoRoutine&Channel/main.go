4efb7a0d1a0135fdd25bf49d64b1776876a28544
TouficGIT <meffe.emilien@gmail.com>
2019-04-17 22:49:32 +0200

    Training: Goroutines

---
+++ Udemy-Go-GoRoutine&Channel/main.go
@@ -0,0 +1,17 @@
+/*
+Concurrence: alternance d'éxecution entre chaque programme, les entités en concurrence essaient d'accèder
+à des données partagés (risques de problèmes)
+
+Ds la plupart des langages, la concurrence est faites avec les threads (fil d'exécution)
+1 process = pls threads
+
+** Race condition : 2 threads essayent de modifier 1 variable en meme tps **
+** Starvation : 1 thread ne relâche jamais son accès à une variable **
+
+*/
+
+package main
+
+func main() {
+
+}
